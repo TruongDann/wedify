740db6a38be6d6505e414b44dd6adcb4019ee8bc
TruongDann <vodan2292@gmail.com>
2026-01-21 21:21:46 +0700

    refactor(editor): refactor code structure for improved readability and maintainability

--- src/constants/cropShapes.ts
+++ src/constants/cropShapes.ts
@@ -1,511 +1,112 @@
-// Crop shapes for image cropping - using image URLs
+// Crop shapes for image cropping - using local images
 export const CROP_SHAPES = [
-  {
-    id: "001",
-    image: "https://assets.cinelove.me/resources/cropShapes/001_ecxcylqi2s.png",
-  },
-  {
-    id: "002",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/002_c193e8ya9w5.png",
-  },
-  {
-    id: "003",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/003_49vdcqwiuqn.png",
-  },
-  {
-    id: "004",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/004_5k90som3oij.png",
-  },
-  {
-    id: "005",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/005_9omz41e7f0o.png",
-  },
-  {
-    id: "006",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/006_1umm2ttl64f.png",
-  },
-  {
-    id: "007",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/007_vg78vz5af0c.png",
-  },
-  {
-    id: "008",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/008_a6rp8l0uurn.png",
-  },
-  {
-    id: "009",
-    image: "https://assets.cinelove.me/resources/cropShapes/009_eho5bulea1.png",
-  },
-  {
-    id: "010",
-    image: "https://assets.cinelove.me/resources/cropShapes/010_tkqncx1mz5.png",
-  },
-  {
-    id: "011",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/011_4uqpcde2764.png",
-  },
-  {
-    id: "012",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/012_zccw91gkz8f.png",
-  },
-  {
-    id: "013",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/013_mjo2qfkce8o.png",
-  },
-  {
-    id: "014",
-    image: "https://assets.cinelove.me/resources/cropShapes/014_jptf4m22xu.png",
-  },
-  {
-    id: "015",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/015_uvy8ypx9frp.png",
-  },
-  {
-    id: "016",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/016_zd65kck1cjb.png",
-  },
-  {
-    id: "017",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/017_qnyb431guuf.png",
-  },
-  {
-    id: "018",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/018_0w37ctc6eac.png",
-  },
-  {
-    id: "019",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/019_mi9d79tbxgo.png",
-  },
-  {
-    id: "020",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/020_gqjxbzqawbf.png",
-  },
-  {
-    id: "021",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/021_mjvsn971lhk.png",
-  },
-  {
-    id: "022",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/022_arw647d7e4q.png",
-  },
-  {
-    id: "023",
-    image: "https://assets.cinelove.me/resources/cropShapes/023_md0ae5zxch.png",
-  },
-  {
-    id: "024",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/024_p0j8m3ca1pc.png",
-  },
-  {
-    id: "025",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/025_me12c2hw5qh.png",
-  },
-  {
-    id: "026",
-    image: "https://assets.cinelove.me/resources/cropShapes/026_zqg274kev4.png",
-  },
-  {
-    id: "027",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/027_xdg8wl79tys.png",
-  },
-  {
-    id: "028",
-    image: "https://assets.cinelove.me/resources/cropShapes/028_s53j8ugaq4.png",
-  },
-  {
-    id: "029",
-    image: "https://assets.cinelove.me/resources/cropShapes/029_grhum0ffpk.png",
-  },
-  {
-    id: "030",
-    image: "https://assets.cinelove.me/resources/cropShapes/030_y7gix26njs.png",
-  },
-  {
-    id: "031",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/031_chdy1yx1u4j.png",
-  },
-  {
-    id: "032",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/032_kjsem5y9r4q.png",
-  },
-  {
-    id: "033",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/033_rfizi5z62v9.png",
-  },
-  {
-    id: "034",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/034_7er2ebvoy29.png",
-  },
-  {
-    id: "035",
-    image: "https://assets.cinelove.me/resources/cropShapes/035_rcpwu1cpk2.png",
-  },
-  {
-    id: "036",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/036_zdeekqbjzri.png",
-  },
-  {
-    id: "037",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/037_q5mcw7wrdbh.png",
-  },
-  {
-    id: "038",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/038_ad97lvv56pc.png",
-  },
-  {
-    id: "039",
-    image: "https://assets.cinelove.me/resources/cropShapes/039_4ee7ilqj5b.png",
-  },
-  {
-    id: "040",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/040_htuf8g2jws7.png",
-  },
-  {
-    id: "041",
-    image: "https://assets.cinelove.me/resources/cropShapes/041_sydx0qwraa.png",
-  },
-  {
-    id: "042",
-    image: "https://assets.cinelove.me/resources/cropShapes/042_rd9cvd4ly6.png",
-  },
-  {
-    id: "043",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/043_n2zqtihf3r8.png",
-  },
-  {
-    id: "044",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/044_o8lonzcwztn.png",
-  },
-  {
-    id: "045",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/045_b07i04zcjvi.png",
-  },
-  {
-    id: "046",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/046_00wrpgmngjgnh.png",
-  },
-  {
-    id: "047",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/047_36xyhgwf6yo.png",
-  },
-  {
-    id: "048",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/048_67of03i2yvl.png",
-  },
-  {
-    id: "049",
-    image: "https://assets.cinelove.me/resources/cropShapes/049_xav4s5nd18.png",
-  },
-  {
-    id: "050",
-    image: "https://assets.cinelove.me/resources/cropShapes/050_ppq1nclz9p.png",
-  },
-  {
-    id: "051",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/051_mhkr4vnv2kn.png",
-  },
-  {
-    id: "052",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/052_ss5x64vti7a.png",
-  },
-  {
-    id: "053",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/053_roofzq7f2mo.png",
-  },
-  {
-    id: "054",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/054_wpku9y0rmba.png",
-  },
-  {
-    id: "055",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/055_hzlgh3hbkgr.png",
-  },
-  {
-    id: "056",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/056_anmfiwnopg5.png",
-  },
-  {
-    id: "057",
-    image: "https://assets.cinelove.me/resources/cropShapes/057_9xby0f47vx.png",
-  },
-  {
-    id: "058",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/058_ffl67098lln.png",
-  },
-  {
-    id: "059",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/059_29f29a7u93q.png",
-  },
-  {
-    id: "060",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/060_humpjyfwnvv.png",
-  },
-  {
-    id: "061",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/061_eheay43znau.png",
-  },
-  {
-    id: "062",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/062_2y48xbvq2r2.png",
-  },
-  {
-    id: "063",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/063_yw6mb2mokja.png",
-  },
-  {
-    id: "064",
-    image: "https://assets.cinelove.me/resources/cropShapes/064_6lgw9ml429.png",
-  },
-  {
-    id: "065",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/065_2h50yo7pc5a.png",
-  },
-  {
-    id: "066",
-    image: "https://assets.cinelove.me/resources/cropShapes/066_p7k9iiv2n6.png",
-  },
-  {
-    id: "067",
-    image: "https://assets.cinelove.me/resources/cropShapes/067_mnk0q2xd3p.png",
-  },
-  {
-    id: "068",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/068_tou9wmnguog.png",
-  },
-  {
-    id: "069",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/069_j38gjjt57gr.png",
-  },
-  {
-    id: "070",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/070_d1r7ntvesiw.png",
-  },
-  {
-    id: "071",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/071_9p8yfjo3alc.png",
-  },
-  {
-    id: "072",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/072_y6myfwhzrlk.png",
-  },
-  {
-    id: "073",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/073_r3mksmjichi.png",
-  },
-  {
-    id: "074",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/074_fhe0tpmemom.png",
-  },
-  {
-    id: "075",
-    image: "https://assets.cinelove.me/resources/cropShapes/075_n8xz6zbogq.png",
-  },
-  {
-    id: "076",
-    image: "https://assets.cinelove.me/resources/cropShapes/076_7orprdx249.png",
-  },
-  {
-    id: "077",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/077_e1egy4ns3ru.png",
-  },
-  {
-    id: "078",
-    image: "https://assets.cinelove.me/resources/cropShapes/078_rbouem298m.png",
-  },
-  {
-    id: "079",
-    image: "https://assets.cinelove.me/resources/cropShapes/079_ok6wgk7o54.png",
-  },
-  {
-    id: "080",
-    image: "https://assets.cinelove.me/resources/cropShapes/080_h4a7utpapu.png",
-  },
-  {
-    id: "081",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/081_q6haxp49lms.png",
-  },
-  {
-    id: "082",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/082_mi0sb5eu6gc.png",
-  },
-  {
-    id: "083",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/083_hlf0d3ymjjw.png",
-  },
-  {
-    id: "084",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/084_qn9cup6lmgh.png",
-  },
-  {
-    id: "085",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/085_bls700dn3b8.png",
-  },
-  {
-    id: "086",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/086_0t54zccuv7i.png",
-  },
-  {
-    id: "087",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/087_2l2wpnvchyv.png",
-  },
-  {
-    id: "088",
-    image: "https://assets.cinelove.me/resources/cropShapes/088_crjne3kr5x.png",
-  },
-  {
-    id: "089",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/089_969dk3xl2sf.png",
-  },
-  {
-    id: "090",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/090_cgdy2my3cmc.png",
-  },
-  {
-    id: "091",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/091_ce8vqqjgmup.png",
-  },
-  {
-    id: "092",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/092_kr1qwaj579g.png",
-  },
-  {
-    id: "093",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/093_k73na041t7a.png",
-  },
-  {
-    id: "094",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/094_iqnqadjpoah.png",
-  },
-  {
-    id: "095",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/095_3ud19ewu5ub.png",
-  },
-  {
-    id: "096",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/096_mok3fotcv1r.png",
-  },
-  {
-    id: "097",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/097_6vkkzomvwvl.png",
-  },
-  {
-    id: "098",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/098_m1sp4jozch8.png",
-  },
-  {
-    id: "099",
-    image: "https://assets.cinelove.me/resources/cropShapes/099_x80gan3kap.png",
-  },
-  {
-    id: "100",
-    image: "https://assets.cinelove.me/resources/cropShapes/100_lc0p7smiwn.png",
-  },
-  {
-    id: "101",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/101_mkqd5xynqqo.png",
-  },
-  {
-    id: "102",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/102_j64nbwt969r.png",
-  },
-  {
-    id: "103",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/103_gesdn6xs5k5.png",
-  },
-  {
-    id: "104",
-    image: "https://assets.cinelove.me/resources/cropShapes/104_t1a0jvv0lj.png",
-  },
-  {
-    id: "105",
-    image: "https://assets.cinelove.me/resources/cropShapes/105_m2v4i9d15y.png",
-  },
-  {
-    id: "106",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/106_ndv4rowwf99.png",
-  },
-  {
-    id: "107",
-    image:
-      "https://assets.cinelove.me/resources/cropShapes/107_o76bqnqx8tl.png",
-  },
+  { id: "001", image: "/cropShapes/shape-001.png" },
+  { id: "002", image: "/cropShapes/shape-002.png" },
+  { id: "003", image: "/cropShapes/shape-003.png" },
+  { id: "004", image: "/cropShapes/shape-004.png" },
+  { id: "005", image: "/cropShapes/shape-005.png" },
+  { id: "006", image: "/cropShapes/shape-006.png" },
+  { id: "007", image: "/cropShapes/shape-007.png" },
+  { id: "008", image: "/cropShapes/shape-008.png" },
+  { id: "009", image: "/cropShapes/shape-009.png" },
+  { id: "010", image: "/cropShapes/shape-010.png" },
+  { id: "011", image: "/cropShapes/shape-011.png" },
+  { id: "012", image: "/cropShapes/shape-012.png" },
+  { id: "013", image: "/cropShapes/shape-013.png" },
+  { id: "014", image: "/cropShapes/shape-014.png" },
+  { id: "015", image: "/cropShapes/shape-015.png" },
+  { id: "016", image: "/cropShapes/shape-016.png" },
+  { id: "017", image: "/cropShapes/shape-017.png" },
+  { id: "018", image: "/cropShapes/shape-018.png" },
+  { id: "019", image: "/cropShapes/shape-019.png" },
+  { id: "020", image: "/cropShapes/shape-020.png" },
+  { id: "021", image: "/cropShapes/shape-021.png" },
+  { id: "022", image: "/cropShapes/shape-022.png" },
+  { id: "023", image: "/cropShapes/shape-023.png" },
+  { id: "024", image: "/cropShapes/shape-024.png" },
+  { id: "025", image: "/cropShapes/shape-025.png" },
+  { id: "026", image: "/cropShapes/shape-026.png" },
+  { id: "027", image: "/cropShapes/shape-027.png" },
+  { id: "028", image: "/cropShapes/shape-028.png" },
+  { id: "029", image: "/cropShapes/shape-029.png" },
+  { id: "030", image: "/cropShapes/shape-030.png" },
+  { id: "031", image: "/cropShapes/shape-031.png" },
+  { id: "032", image: "/cropShapes/shape-032.png" },
+  { id: "033", image: "/cropShapes/shape-033.png" },
+  { id: "034", image: "/cropShapes/shape-034.png" },
+  { id: "035", image: "/cropShapes/shape-035.png" },
+  { id: "036", image: "/cropShapes/shape-036.png" },
+  { id: "037", image: "/cropShapes/shape-037.png" },
+  { id: "038", image: "/cropShapes/shape-038.png" },
+  { id: "039", image: "/cropShapes/shape-039.png" },
+  { id: "040", image: "/cropShapes/shape-040.png" },
+  { id: "041", image: "/cropShapes/shape-041.png" },
+  { id: "042", image: "/cropShapes/shape-042.png" },
+  { id: "043", image: "/cropShapes/shape-043.png" },
+  { id: "044", image: "/cropShapes/shape-044.png" },
+  { id: "045", image: "/cropShapes/shape-045.png" },
+  { id: "046", image: "/cropShapes/shape-046.png" },
+  { id: "047", image: "/cropShapes/shape-047.png" },
+  { id: "048", image: "/cropShapes/shape-048.png" },
+  { id: "049", image: "/cropShapes/shape-049.png" },
+  { id: "050", image: "/cropShapes/shape-050.png" },
+  { id: "051", image: "/cropShapes/shape-051.png" },
+  { id: "052", image: "/cropShapes/shape-052.png" },
+  { id: "053", image: "/cropShapes/shape-053.png" },
+  { id: "054", image: "/cropShapes/shape-054.png" },
+  { id: "055", image: "/cropShapes/shape-055.png" },
+  { id: "056", image: "/cropShapes/shape-056.png" },
+  { id: "057", image: "/cropShapes/shape-057.png" },
+  { id: "058", image: "/cropShapes/shape-058.png" },
+  { id: "059", image: "/cropShapes/shape-059.png" },
+  { id: "060", image: "/cropShapes/shape-060.png" },
+  { id: "061", image: "/cropShapes/shape-061.png" },
+  { id: "062", image: "/cropShapes/shape-062.png" },
+  { id: "063", image: "/cropShapes/shape-063.png" },
+  { id: "064", image: "/cropShapes/shape-064.png" },
+  { id: "065", image: "/cropShapes/shape-065.png" },
+  { id: "066", image: "/cropShapes/shape-066.png" },
+  { id: "067", image: "/cropShapes/shape-067.png" },
+  { id: "068", image: "/cropShapes/shape-068.png" },
+  { id: "069", image: "/cropShapes/shape-069.png" },
+  { id: "070", image: "/cropShapes/shape-070.png" },
+  { id: "071", image: "/cropShapes/shape-071.png" },
+  { id: "072", image: "/cropShapes/shape-072.png" },
+  { id: "073", image: "/cropShapes/shape-073.png" },
+  { id: "074", image: "/cropShapes/shape-074.png" },
+  { id: "075", image: "/cropShapes/shape-075.png" },
+  { id: "076", image: "/cropShapes/shape-076.png" },
+  { id: "077", image: "/cropShapes/shape-077.png" },
+  { id: "078", image: "/cropShapes/shape-078.png" },
+  { id: "079", image: "/cropShapes/shape-079.png" },
+  { id: "080", image: "/cropShapes/shape-080.png" },
+  { id: "081", image: "/cropShapes/shape-081.png" },
+  { id: "082", image: "/cropShapes/shape-082.png" },
+  { id: "083", image: "/cropShapes/shape-083.png" },
+  { id: "084", image: "/cropShapes/shape-084.png" },
+  { id: "085", image: "/cropShapes/shape-085.png" },
+  { id: "086", image: "/cropShapes/shape-086.png" },
+  { id: "087", image: "/cropShapes/shape-087.png" },
+  { id: "088", image: "/cropShapes/shape-088.png" },
+  { id: "089", image: "/cropShapes/shape-089.png" },
+  { id: "090", image: "/cropShapes/shape-090.png" },
+  { id: "091", image: "/cropShapes/shape-091.png" },
+  { id: "092", image: "/cropShapes/shape-092.png" },
+  { id: "093", image: "/cropShapes/shape-093.png" },
+  { id: "094", image: "/cropShapes/shape-094.png" },
+  { id: "095", image: "/cropShapes/shape-095.png" },
+  { id: "096", image: "/cropShapes/shape-096.png" },
+  { id: "097", image: "/cropShapes/shape-097.png" },
+  { id: "098", image: "/cropShapes/shape-098.png" },
+  { id: "099", image: "/cropShapes/shape-099.png" },
+  { id: "100", image: "/cropShapes/shape-100.png" },
+  { id: "101", image: "/cropShapes/shape-101.png" },
+  { id: "102", image: "/cropShapes/shape-102.png" },
+  { id: "103", image: "/cropShapes/shape-103.png" },
+  { id: "104", image: "/cropShapes/shape-104.png" },
+  { id: "105", image: "/cropShapes/shape-105.png" },
+  { id: "106", image: "/cropShapes/shape-106.png" },
+  { id: "107", image: "/cropShapes/shape-107.png" },
 ];
 
 export const ASPECT_RATIOS = [
